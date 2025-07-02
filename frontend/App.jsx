68faca4f478d6b8f21ad31a54a5977eeb56fc8f1
Nikitanamdeo1212 <nikitanamdeo40@gmail.com>
2025-07-02 07:41:36 +0000

    Added frontend Vite + Tailwind setup in separate folder

---
+++ frontend/App.jsx
@@ -0,0 +1,14 @@
+
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+    <div className="bg-gray-100 dark:bg-gray-950 min-h-screen text-black dark:text-white">
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
